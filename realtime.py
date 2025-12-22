efa9879cea879c3f24b888d83a0f28d7bf08034a
Muhammad Ehtesham Safeer <77203503+ehteshamsafeer@users.noreply.github.com>
2025-12-22 14:56:13 +0500

    Fix print statement for OPENAI_API_KEY

--- realtime.py
+++ realtime.py
@@ -8,7 +8,7 @@ class RealtimeAI:
 
     async def connect(self):
         OPENAI_API_KEY="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"
-        print("🔑 OPENAI_API_KEY detected:", OPENAI_API_KEY))
+        print("🔑 OPENAI_API_KEY detected:", OPENAI_API_KEY)
         self.ws = await websockets.connect(
     "wss://api.openai.com/v1/realtime"
     "?model=gpt-realtime-mini-2025-12-15",
